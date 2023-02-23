a421083def4a04e209452a6e03612ef046272234
candiceboutet <candice.boutet@gmail.com>
2023-02-23 15:30:51 +0100

    feat(Toggle): creation of a dropdown menu

--- front/src/components/Toggle/index.js
+++ front/src/components/Toggle/index.js
@@ -1,15 +1,15 @@
 import { NavLink } from 'react-router-dom';
-import { useState } from 'react';
-
-import './styles.scss';
 
 const Toggle = () => {
-    
-    return (
-        <div className="menu">
-<NavLink to="/my-space" className="space">Mon espace </NavLink>
-<NavLink to="/" className="space">Déconnexion </NavLink>
-        </div>
-    )}
+
+
+  return (
+
+    <div className="menu">
+        <NavLink to="/" className="space">Mon espace </NavLink>
+        <NavLink to="/" className="space">Déconnexion </NavLink>
+  </div>  
+  );
+}
 
     export default Toggle;
