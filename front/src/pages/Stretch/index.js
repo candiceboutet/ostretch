15493cf6a4ade2784c1d33081ca65acf08b8fa2b
Candice Boutet <74901679+candiceboutet@users.noreply.github.com>
2023-03-07 14:45:19 +0100

    feat(Stretch): delete & edit stretch (#26)
    * refacto(my-space)
    
    * test
    
    * feat(InfoForm): patch working
    
    * style(InfoForm)
    
    * feat(Bookmarks): delete favorites working
    
    * refacto(Bookmarks): style
    
    * feat(My-space): delete user + bug fixed
    
    * .
    
    * feat(Stretch): delete & edit stretch

--- front/src/pages/Stretch/index.js
+++ front/src/pages/Stretch/index.js
@@ -1,13 +1,21 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
 
+import { AiOutlineHeart, AiFillHeart, AiOutlineDelete, AiOutlineEdit } from 'react-icons/ai';
 // Styles
 import './styles.scss'
+import StretchForm from "../../components/StretchForm";
 
-export default function Stretch() {
+const Stretch = ({isLogged, isAdmin}) => {
+    const navigate = useNavigate();
     const { id } = useParams();
     const [stretch, setStretch] = useState({});
+    const [isFavorite, setIsFavorite] = useState(false);
+    const [onEdit, setOnEdit] = useState(false);
+    const token = localStorage.getItem('token');
+    console.log(stretch)
 
     useEffect(() => {
         axios.get(`http://localhost:3000/stretches/${id}`)
@@ -15,6 +23,38 @@ export default function Stretch() {
                 setStretch(response.data)
             })
     }, [id])
+
+    const handleFavorite = (event) => {
+        event.preventDefault();
+
+        setIsFavorite(!isFavorite);
+        const config = {
+            headers: { Authorization: `Bearer ${token}` }
+          };
+          console.log(config);
+          axios.post(`http://localhost:3000/user/me/stretches/${id}`,{}, config)
+            .then(response => {
+              setIsFavorite(true);
+            })
+            .catch(error => {
+              console.log(error);
+            }, [token]);
+    };
+    const handleDelete = () => {
+        const config = {
+            headers: { Authorization: `Bearer ${token}` }
+          };
+          axios.delete(`http://localhost:3000/stretches/${id}`, config)
+          .then(response => {
+            navigate("/stretches")
+          })
+          .catch(error => {
+            console.log(error);
+          });
+    }
+    const handleEdit = () => {
+        setOnEdit(!onEdit);
+    }
 
     return (
         <div className="Stretch">
@@ -26,12 +66,28 @@ export default function Stretch() {
                         title={stretch.title}
                         className="stretch-image"
                     />
+                {isLogged ? (
+                <span onClick={handleFavorite} className="favorite-icon">
+                { isFavorite ? <AiFillHeart className='filled-icon' /> : <AiOutlineHeart />}
+                </span>) : null
+                }
+
                 </div>
-                <div className="stretch-infos">
+                {onEdit ? 
+                <StretchForm stretch={stretch} setStretch={setStretch} id={id} setOnEdit={setOnEdit}/> : 
+                (<div className="stretch-infos">
                     <h1>{stretch.title}</h1>
                     <p>{stretch.description_content}</p>
-                </div>
+                    {isAdmin ? (
+                                <div className="icon-box">
+                                <AiOutlineEdit onClick={handleEdit} />
+                                <AiOutlineDelete onClick={handleDelete} />
+                                </div>
+                ) : null }
+                </div> )}
             </div>
         </div>
     )
 }
+
+export default Stretch;
