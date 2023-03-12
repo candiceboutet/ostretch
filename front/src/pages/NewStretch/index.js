f3a03681c794a7bd8d5859babd17b8ab4ed70799
Fred Meziere <85186423+FredMeziere@users.noreply.github.com>
2023-03-12 22:10:00 +0100

    merge 3 test (#41)
    * merge dev to main2
    
    * merge dev to main 3

--- front/src/pages/NewStretch/index.js
+++ front/src/pages/NewStretch/index.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-import './styles.css'
+import './styles.scss'
 
 
 const NewStretch = () => {
